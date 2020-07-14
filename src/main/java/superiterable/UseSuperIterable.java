0d0f9d98625d528c95e0e56ba14f08a60b9a7c0a
Simon Roberts <simon@dancingcloudservices.com>
2020-07-14 13:19:29 -0600

    Day one Break three

--- src/main/java/superiterable/UseSuperIterable.java
+++ src/main/java/superiterable/UseSuperIterable.java
@@ -2,6 +2,9 @@ package superiterable;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class UseSuperIterable {
   public static void main(String[] args) {
@@ -11,19 +14,22 @@ public class UseSuperIterable {
 
     sis
         .filter(s -> s.length() > 3)
-        .map(s -> s.length())
+//      .map(s -> s.length())
+        .map(String::length)
         .forEach(s -> System.out.println("> " + s));
 
     sis
         .map(s -> "Welcome " + s.toUpperCase())
         .forEach(s -> System.out.println(s));
 
-    SuperIterable<Student> roster = new SuperIterable<>(Arrays.asList(
+    List<Student> rosterList = Arrays.asList(
         Student.of("Fred", 70, "Math", "Physics"),
         Student.of("Jim", 58, "Art"),
         Student.of("Sheila", 92,
             "Math", "Physics", "Astrophysics", "Quantum Mechanics")
-    ));
+    );
+
+    SuperIterable<Student> roster = new SuperIterable<>(rosterList);
 
     roster
         .filter(s -> s.getGrade() > 60)
@@ -33,6 +39,37 @@ public class UseSuperIterable {
 
     roster
         .flatMap(s -> new SuperIterable<>(s.getCourses()))
+//        .forEach(s -> System.out.println(s))
+    ;
+
+    Function<Student, String> fss = s -> {
+      System.out.println("transformer...");
+      return s.getName() + " has grade " + s.getGrade();
+    };
+
+    rosterList.stream()
+        .flatMap(s -> s.getCourses().stream())
         .forEach(s -> System.out.println(s));
+
+    System.out.println("------------- super iterable ------------");
+    SuperIterable<String> sistr = roster
+        .map(fss)
+//        .forEach(s -> System.out.println(s))
+    ;
+
+    sistr.forEach(s -> System.out.println(s));
+    System.out.println("---");
+    sistr.forEach(s -> System.out.println(s));
+
+    System.out.println("------------- stream ------------");
+    List<String> ss = rosterList.stream()
+        .map(fss)
+//        .forEach(s -> System.out.println(s))
+        .collect(Collectors.toList());
+    ;
+
+    ss.stream().forEach(s -> System.out.println(s));
+    ss.stream().forEach(System.out::println);
+
   }
 }
