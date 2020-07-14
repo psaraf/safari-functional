6d062962b402b61e0d98ded45f0076874bd1761b
Simon Roberts <simon@dancingcloudservices.com>
2020-07-14 11:03:02 -0600

    Day One Break One

---
+++ src/main/java/students/School.java
@@ -0,0 +1,100 @@
+package students;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+interface StudentCriterion {
+  boolean test(Student s);
+  default void doStuff() {};
+}
+
+class SmartCriterion implements StudentCriterion {
+  @Override
+  public boolean test(Student s) {
+    return s.getGrade() > 65;
+  }
+//  public void doStuff() {}
+}
+
+class EnthusiasticCriterion implements StudentCriterion {
+  @Override
+  public boolean test(Student s) {
+    return s.getCourses().size() > 3;
+  }
+}
+
+public class School {
+  public static void showAll(List<Student> ls) {
+    for (Student s : ls) {
+      System.out.println("> " + s);
+    }
+    System.out.println("=====================");
+  }
+
+  public static List<Student> getStudentsByCriterion(List<Student> ls,
+                                                     StudentCriterion crit) {
+    List<Student> out = new ArrayList<>();
+    for (Student s : ls) {
+      if (crit.test(s)) {
+        out.add(s);
+      }
+    }
+    return out;
+  }
+
+//  public static List<Student> getSmartStudents(List<Student> ls, int threshold) {
+//    List<Student> out = new ArrayList<>();
+//    for (Student s : ls) {
+//      if (s.getGrade() > threshold) {
+//        out.add(s);
+//      }
+//    }
+//    return out;
+//  }
+//
+//  public static List<Student> getEnthusiasticStudents(List<Student> ls, int threshold) {
+//    List<Student> out = new ArrayList<>();
+//    for (Student s : ls) {
+//      if (s.getCourses().size() > threshold) {
+//        out.add(s);
+//      }
+//    }
+//    return out;
+//  }
+
+  public static void main(String[] args) {
+    List<Student> roster = Arrays.asList(
+        Student.of("Fred", 70, "Math", "Physics"),
+        Student.of("Jim", 58, "Art"),
+        Student.of("Sheila", 92,
+            "Math", "Physics", "Astrophysics", "Quantum Mechanics")
+    );
+//    showAll(getSmartStudents(roster, 65));
+//    showAll(getEnthusiasticStudents(roster, 3));
+
+    // command pattern (OO)
+    // "Higher order function" (FP)
+    showAll(getStudentsByCriterion(roster, new SmartCriterion()));
+    showAll(getStudentsByCriterion(roster, new EnthusiasticCriterion()));
+    // lambda expression (version 1)
+//    showAll(getStudentsByCriterion(roster, (Student s) -> {
+//      return s.getGrade() < 65;
+//    } ));
+//    showAll(getStudentsByCriterion(roster, (s) -> {
+//      return s.getGrade() < 65;
+//    } ));
+    // block lambda
+//    showAll(getStudentsByCriterion(roster, s -> {
+//      return s.getGrade() < 65;
+//    } ));
+    // Expression lambda
+    showAll(getStudentsByCriterion(roster, s -> s.getGrade() < 65));
+
+    // NOT ALLOWED
+//    Object criterion = s -> s.getGrade() < 65;
+    StudentCriterion criterion = s -> s.getGrade() < 65;
+    System.out.println("Class name of this lambda is " +
+        criterion.getClass().getName());
+  }
+}
---
+++ src/main/java/students/Student.java
@@ -0,0 +1,48 @@
+package students;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class Student {
+  private String name;
+  private int grade;
+  private List<String> courses;
+
+  private Student(String name, int grade, List<String> courses) {
+    this.name = name;
+    this.grade = grade;
+    this.courses = courses;
+  }
+
+  public static Student of(String name, int grade, String ... courses) {
+    return new Student(name, grade,
+        Collections.unmodifiableList(Arrays.asList(courses)));
+                                  // List.of truly immutable list :)
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public int getGrade() {
+    return grade;
+  }
+
+  public Student setGrade(int g) {
+    return new Student(this.name, g, this.courses);
+  }
+
+  public List<String> getCourses() {
+    return courses;
+  }
+
+  @Override
+  public String toString() {
+    return "Student{" +
+        "name='" + name + '\'' +
+        ", grade=" + grade +
+        ", courses=" + courses +
+        '}';
+  }
+}
